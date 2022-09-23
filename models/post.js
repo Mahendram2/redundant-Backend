766197597dcf34a1e28e2b8c9e9e50f08d0f888c
Seth Miller <millerado@gmail.com>
2022-09-23 08:52:19 -0600

    syntax fixes

--- models/post.js
+++ models/post.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema ({
+const postSchema = new Schema(
+  {
     title: String,
-    createdby: String
-
-
-}, {timestamps: true});
+    createdBy: String,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Post', postSchema);
