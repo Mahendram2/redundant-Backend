07bc28111437b523a3da6b315c05a41472b2310e
Levonte Larry <levontelarry@Levontes-MacBook-Air.local>
2022-09-25 11:57:08 -0400

    schema, embed

--- models/user.js
+++ models/user.js
@@ -5,15 +5,9 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     username: { type: String, required: true },
-<<<<<<< HEAD
-    image: {type: String, default: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'},
-=======
-    image: {
-      type: String,
-      default:
-        'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
-    },
->>>>>>> 36d1f4a1a160650ac32f8d161d32aa06bdd825b6
+
+    image: {type: String, default:'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'},
+    
     bio: String,
   },
   { timestamps: true }
